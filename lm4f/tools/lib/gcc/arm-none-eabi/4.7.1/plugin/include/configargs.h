38eb874aa22c24850c46942774491960968220a4
Stephen Crane <jscrane@gmail.com>
2014-11-07 12:08:37 +0000

    remove tools

--- lm4f/tools/lib/gcc/arm-none-eabi/4.7.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-4.7.1/configure --target=arm-none-eabi --prefix=/home2/kimballr/yagarto/tools/lm4f --disable-shared --disable-nls --disable-threads --disable-libssp --disable-libstdcxx-pch --disable-libmudflap --disable-libgomp -v --enable-languages=c,c++ --enable-interwork --enable-multilib --with-gcc --with-gnu-ld --with-gnu-as --with-dwarf2 --with-newlib --with-headers=../newlib-1.20.0/newlib/libc/include --with-mpc=/home2/kimballr/yagarto/addontools --with-mpfr=/home2/kimballr/yagarto/addontools --with-gmp=/home2/kimballr/yagarto/addontools --with-system-zlib";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
